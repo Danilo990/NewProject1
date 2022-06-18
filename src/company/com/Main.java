c499195b22d47fbd9bdf34c82f499ba8039c41d3
Danilo990 <danilocvetkovikj90@gmail.com>
2022-06-18 23:50:15 +0200

    Add project on github

---
+++ src/company/com/Main.java
@@ -0,0 +1,8 @@
+package company.com;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("add and clone ");
+        //add this project on github and clone
+    }
+}
